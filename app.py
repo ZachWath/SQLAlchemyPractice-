d40550a6984a44515e7be820fa9d0bd1eda9c55b
ZachWath <89270314+ZachWath@users.noreply.github.com>
2021-09-24 23:07:44 -0500

    added database model

--- app.py
+++ app.py
@@ -3,7 +3,22 @@ from models import (Base, session, Book, engine)
 
 
 
-# import models
+def menu():
+    while True:
+        print('''
+            \nProgramming Books:
+            \r1) Add book
+            \r2) View all books
+            \r3) Search for book
+            \r4) Book analysis
+            \r5) exit''')
+        choice = input ('What would you like to do?   ')
+        if choice in ['1','2','3','4','5']:
+            return choice
+        else:
+            input('''please choose one of the options above.
+                    A number 1-5
+                    press enter to try again   ''')
 # main menu -add , search , analysis , exit, view
 # add books to database
 # edit books
@@ -16,3 +31,4 @@ from models import (Base, session, Book, engine)
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    menu()
